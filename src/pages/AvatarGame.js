f80b6cc4b63b24b3ce434902834323510451c1fa
Jack Herdman <jack.herdman3@studytafensw.edu.au>
2020-08-02 22:31:57 +1000

    fixes spelling errors

--- src/pages/AvatarGame.js
+++ src/pages/AvatarGame.js
@@ -22,9 +22,9 @@ export default function AvatarGame() {
         <div className="bg-dark vh-100" style={{ marginTop: "69px" }}>
             <Container>
                 <h3 className="py-2 px-5 mb-0 text-light">Avatar Match Game</h3>
-                <div className="py-2 px-2 mb-3 mt-0 text-light">I am incredibly proud of this game! It is a part of our first HTML website project. The design is inspired by the Pathfinder quest in Destiny 2 and skinned with the 4 nations from the tv show Avatar: The Last Airbender. The project works with javascript controlling elements by ID and using onclick events to change their source. This was very difficult work that i had the help of both the Tafe teach and Trent on but majority of the code was my work.<br /><br />On load, the page will generate  the name one of four random nations in the Avatar World. When you click on an image, it will change all images in the same row and column. Your goal is to get all 9 images to match the displayed nation.</div>
+                <div className="py-2 px-2 mb-3 mt-0 text-light">I am incredibly proud of this game! It is a part of our first HTML website project. The design is inspired by the Pathfinder quest in Destiny 2 and skinned with the 4 nations from the tv show Avatar: The Last Airbender. The project works with javascript controlling elements by ID and using onclick events to change their source. This was very difficult work that i had the help of both the TAFE teacher and Trent on but majority of the code was my work.<br /><br />On load, the page will generate the name one of four random nations in the Avatar World. When you click on an image, it will change all adjacent images. Your goal is to get all 9 images to match the displayed nation.</div>
 
-                <div className="py-2 px-2 mb-3 mt-0 text-light">The Next Avatar is from the <span id= "nation" style={{fontSize: "larger", textDecoration: "underline"}}></span>. make all elements match.</div>
+                <div className="py-2 px-2 mb-3 mt-0 text-light">The Next Avatar is from the <span id= "nation" style={{fontSize: "larger", textDecoration: "underline"}}></span>. Make all elements match.</div>
 
                 <div id="gameSquare">
                     <div id="rowOne">
