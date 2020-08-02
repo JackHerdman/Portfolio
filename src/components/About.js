f18e674f02b8d5d6e49ec0836aae8cbe4d0eaef4
Jack Herdman <jack.herdman3@studytafensw.edu.au>
2020-08-02 22:35:43 +1000

    fixes more spelling mistakes

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
             <div  id="about" style={{ position: "relative", top: "-227px" }}></div>
             <div className="py-5 px-5 text-light" style={{ backgroundColor: `rgba(${52}, ${58}, ${64})` }}>
                 <p style={{ fontWeight: "400", lineHeight: "1.2", fontSize: "30px", color: `rgba(${248}, ${249}, ${250})` }}>    
-                Hey, my name is Jack! I'm new to coding but so far, am enjoying the challenges of learning new technologies. I have worked at Service NSW for about 4 years starting in the Digital Stores before moving into a centre as a DSR. I'm looking forward to working behind the scenes to help create new online capabilites to help the wider NSW community. I have interests in gaming (mainly PS4), popular culture and reading. I also on occasion take a half decent picture! You can see some of my photos in the gallery.</p>
+                Hey, my name is Jack! I'm new to coding but so far, am enjoying the challenges of learning new technologies. I have worked at Service NSW for about 4 years starting in the Digital Stores before moving into a centre as a DSR. I'm looking forward to working behind the scenes to help create new online capabilities to help the wider NSW community. I have interests in gaming (mainly PS4), popular culture and reading. I also on occasion take a half decent picture! You can see some of my photos in the gallery.</p>
             </div>
         </section>
     )
